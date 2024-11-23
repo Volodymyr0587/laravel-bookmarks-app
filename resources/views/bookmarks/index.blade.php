503e95093e67a365e9618548b138c1d8e8782877
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-23 20:14:27 +0200

    style index page

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -14,22 +14,20 @@
 
                     <div class="mt-12">
                         @foreach ($bookmarks as $bookmark)
-                            <div>
+                            <div class="grid grid-cols-4 items-center my-4">
+
                                 <img src="{{ $bookmark->image ? asset('storage/' . $bookmark->image) : asset('images/default.jpg') }}"
-                                alt="{{ $bookmark->title }}"
-                                class="relative inline-block h-20 w-20 !rounded-md object-cover object-center" />
+                                    alt="{{ $bookmark->title }}"
+                                    class="relative inline-block h-20 w-20 !rounded-md object-cover object-center" />
+
+                                <p class="text-lg font-bold">{{ $bookmark->name }}</p>
+
+                                <x-primary-link class="mr-4 bg-blue-900 hover:bg-blue-700" route="{{ route('bookmarks.show', $bookmark) }}">View</x-primary-link>
+                                <x-primary-link class="mr-4 bg-green-900 hover:bg-green-700" route="{{ route('bookmarks.edit', $bookmark) }}">Edit</x-primary-link>
 
                             </div>
-                            <ul>
-                                <li>
-                                    <a href="{{ route('bookmarks.show', $bookmark) }}">{{ $bookmark->name }}</a>
-                                </li>
-                                <li>{{ $bookmark->url }}</li>
-                            </ul>
                         @endforeach
                     </div>
-
-
                 </div>
             </div>
         </div>
--- resources/views/bookmarks/show.blade.php
+++ resources/views/bookmarks/show.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    <x-primary-link route="{{ route('bookmarks.edit', $bookmark) }}">Edit bookmark</x-primary-link>
+                    <x-primary-link route="{{ route('bookmarks.index') }}">All bookmarks</x-primary-link>
 
                     <div class="mt-12">
 
