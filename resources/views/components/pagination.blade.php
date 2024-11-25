310f048e63d62556179aa811e327e067bcd6fe12
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-11-25 15:32:24 +0200

    limit the number of page links displayed: dynamically calculate a range of pages to show around the current page while still displaying the first, last, and ellipsis (...) where appropriate

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,5 +1,16 @@
 @props(['resource'])
 
+@props(['resource'])
+
+@php
+    // Calculate range of pages to show
+    $currentPage = $resource->currentPage();
+    $lastPage = $resource->lastPage();
+    $adjacent = 2; // Number of pages to show on either side of the current page
+    $startPage = max(1, $currentPage - $adjacent);
+    $endPage = min($lastPage, $currentPage + $adjacent);
+@endphp
+
 <div class="flex items-center justify-between pb-6 px-4">
     {{-- Previous Page Button --}}
     @if ($resource->onFirstPage())
@@ -17,8 +28,22 @@
 
     {{-- Page Numbers --}}
     <div class="flex items-center gap-2">
-        @foreach(range(1, $resource->lastPage()) as $page)
-            @if ($page == $resource->currentPage())
+        {{-- First Page --}}
+        @if ($startPage > 1)
+            <a href="{{ $resource->url(1) }}"
+                class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10">
+                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">1</span>
+            </a>
+            @if ($startPage > 2)
+                <span class="relative h-8 max-h-[32px] w-8 max-w-[32px] text-center align-middle font-sans text-xs font-medium text-gray-500">
+                    ...
+                </span>
+            @endif
+        @endif
+
+        {{-- Middle Pages --}}
+        @foreach (range($startPage, $endPage) as $page)
+            @if ($page == $currentPage)
                 <button
                     class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg border border-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     disabled>
@@ -35,6 +60,19 @@
                 </a>
             @endif
         @endforeach
+
+        {{-- Last Page --}}
+        @if ($endPage < $lastPage)
+            @if ($endPage < $lastPage - 1)
+                <span class="relative h-8 max-h-[32px] w-8 max-w-[32px] text-center align-middle font-sans text-xs font-medium text-gray-500">
+                    ...
+                </span>
+            @endif
+            <a href="{{ $resource->url($lastPage) }}"
+                class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10">
+                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">{{ $lastPage }}</span>
+            </a>
+        @endif
     </div>
 
     {{-- Next Page Button --}}
@@ -51,3 +89,4 @@
         </button>
     @endif
 </div>
+
